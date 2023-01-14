561447ce0a346adb7548c5c2e067cf3ed7f3c52f
Jay Patel <68432630+JP8433@users.noreply.github.com>
2023-01-14 19:10:49 +0530

    Create oddeven.js

---
+++ oddeven.js
@@ -0,0 +1,10 @@
+function checkEvenOdd(num) {
+  if (num % 2 === 0) {
+    console.log(num + " is even.");
+  } else {
+    console.log(num + " is odd.");
+  }
+}
+
+checkEvenOdd(2); // 2 is even.
+checkEvenOdd(3); // 3 is odd.
